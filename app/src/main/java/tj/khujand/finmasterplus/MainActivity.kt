b4818e438247bb0f5ce59c85904fded576411fc5
nazirjon <developeranditservice@gmail.com>
2024-06-03 20:36:02 +0500

    success

--- app/src/main/java/tj/khujand/finmasterplus/MainActivity.kt
+++ app/src/main/java/tj/khujand/finmasterplus/MainActivity.kt
@@ -1,71 +1,36 @@
 package tj.khujand.finmasterplus
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
-import tj.khujand.finmasterplus.ui.history.HistoryFragment
-import tj.khujand.finmasterplus.ui.home.HomeFragment
-import tj.khujand.finmasterplus.ui.profil.ProfilFragment
-import tj.khujand.finmasterplus.ui.report.ReportFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import tj.khujand.finmasterplus.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
-    private lateinit var bottomNavigationView: BottomNavigationView
-    private val previousFragment = arrayListOf(0)
+class MainActivity : AppCompatActivity() {
+    private lateinit var bind: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        bottomNavigationView = findViewById(R.id.bottomNavMenu)
-        bottomNavigationView.selectedItemId = R.id.home
-    }
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        supportFragmentManager.beginTransaction().apply {
+        // Инициализация NavHostFragment и NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frgChanger) as? NavHostFragment
+            ?: throw IllegalStateException("NavHostFragment is not found")
+        navController = navHostFragment.navController
 
-            fun setCustomAnimRtoL() {
-                setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
-            }
+        // Настройка BottomNavigationView с NavController
+        bind.bottomNavigationView.setupWithNavController(navController)
+        bind.bottomNavigationView.background=null
+        
 
-            fun setCustomAnimLtoR() {
-                setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right)
-            }
-
-            previousFragment.add(item.itemId)
-            previousFragment.reverse()
-
-            if (previousFragment[1] == R.id.home && (item.itemId == R.id.history || item.itemId == R.id.reports || item.itemId == R.id.profile)) {
-                setCustomAnimRtoL()
-            } else if (previousFragment[1] == R.id.history && (item.itemId == R.id.reports || item.itemId == R.id.profile)) {
-                setCustomAnimRtoL()
-            } else if (previousFragment[1] == R.id.history && item.itemId == R.id.home) {
-                setCustomAnimLtoR()
-            } else if (previousFragment[1] == R.id.reports && item.itemId == R.id.profile) {
-                setCustomAnimRtoL()
-            } else if (previousFragment[1] == R.id.reports && (item.itemId == R.id.home || item.itemId == R.id.history)) {
-                setCustomAnimLtoR()
-            } else if (previousFragment[1] == R.id.profile && (item.itemId == R.id.history || item.itemId == R.id.reports || item.itemId == R.id.home)) {
-                setCustomAnimLtoR()
-            } else {
-                setCustomAnimations(0, 0)
-            }
-            if (previousFragment[1] != item.itemId) {
-
-                when (item.itemId) {
-                    R.id.home -> replace(R.id.frgChanger, HomeFragment())
-                    R.id.history -> replace(R.id.frgChanger, HistoryFragment())
-                    R.id.reports -> replace(R.id.frgChanger, ReportFragment())
-                    R.id.profile -> replace(R.id.frgChanger, ProfilFragment())
-                }
-                commit()
-            }
+        // Привязка FAB к BottomAppBar
+        bind.fab.setOnClickListener {
+            // Ваш код для действия при нажатии на FAB
         }
-        previousFragment.reverse()
-        return true
     }
 }
